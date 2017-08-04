3523d51228000f35571ef95ec6f78b689e875c6d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-08-04 16:32:52 -0300

    fix include dir

--- conanfile.py
+++ conanfile.py
@@ -32,11 +32,14 @@ class BitprimconsensusConan(ConanFile):
 
     def package(self):
         self.copy("*.h", dst="include", src="include")
+        self.copy("*.hpp", dst="include", src="include")
+        self.copy("*.ipp", dst="include", src="include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+
 
     def package_info(self):
         self.cpp_info.includedirs = ['include']
