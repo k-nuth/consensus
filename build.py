69e160c25798ec564faf13f0c8c8218dc3eca8dd
dramos <dario.fenix@gmail.com>
2017-08-18 10:54:29 -0300

    DEV-10: Tag c++11 builds [skip appveyor]

--- build.py
+++ build.py
@@ -3,15 +3,14 @@ import os
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="testing",
-                                 remotes="https://api.bintray.com/conan/bitprim/bitprim")
+                                 remotes="https://api.bintray.com/conan/bitprim/bitprim",
+                                 archs=["x86_64"])
 
-    builder.add_common_builds(shared_option_name="bitprim-consensus:shared")
-    # builder.password = os.getenv("CONAN_PASSWORD")
+    builder.add_common_builds(shared_option_name="bitprim-consensus:shared", pure_c=False)
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
-                and settings["arch"] == "x86_64" \
                 and options["bitprim-consensus:shared"] == False:
             filtered_builds.append([settings, options, env_vars, build_requires])
 
