9833806d985eff92d55469d4ef8510055cee99bf
dario_ramos <Dario.Ramos@tellago.com>
2017-08-18 15:36:47 -0300

    DEV-10: pure_c was not needed

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
                                  remotes="https://api.bintray.com/conan/bitprim/bitprim",
                                  archs=["x86_64"])
 
-    builder.add_common_builds(shared_option_name="bitprim-consensus:shared", pure_c=False)
+    builder.add_common_builds(shared_option_name="bitprim-consensus:shared")
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
