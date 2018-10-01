4f6e4e22e97e89e52497e1e93d96942e9df49753
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-10-01 17:42:39 -0300

    compiler flags

--- conanfile.py
+++ conanfile.py
@@ -42,7 +42,9 @@ class BitprimConsensusConan(BitprimConanFile):
                "microarchitecture": "ANY", #["x86_64", "haswell", "ivybridge", "sandybridge", "bulldozer", ...]
                "fix_march": [True, False],
                "verbose": [True, False],
-               "use_domain": [True, False]
+               "use_domain": [True, False],
+               "cxxflags": "ANY",
+               "cflags": "ANY",
     }
 
     default_options = "shared=False", \
@@ -54,7 +56,9 @@ class BitprimConsensusConan(BitprimConanFile):
         "microarchitecture=_DUMMY_",  \
         "fix_march=False", \
         "verbose=False", \
-        "use_domain=False"
+        "use_domain=False", \
+        "cxxflags=_DUMMY_", \
+        "cflags=_DUMMY_"
 
     generators = "cmake"
     exports = "conan_*", "ci_utils/*"
@@ -103,6 +107,8 @@ class BitprimConsensusConan(BitprimConanFile):
         self.info.options.with_python = "ANY"
         self.info.options.verbose = "ANY"
         self.info.options.fix_march = "ANY"
+        self.info.options.cxxflags = "ANY"
+        self.info.options.cflags = "ANY"
 
         #For Bitprim Packages libstdc++ and libstdc++11 are the same
         if self.settings.compiler == "gcc" or self.settings.compiler == "clang":
@@ -129,6 +135,12 @@ class BitprimConsensusConan(BitprimConanFile):
 
         if self.settings.compiler == "Visual Studio":
             cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " /DBOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE"
+
+        if self.options.cxxflags != "_DUMMY_":
+            cmake.definitions["CONAN_CXX_FLAGS"] = cmake.definitions.get("CONAN_CXX_FLAGS", "") + " " + str(self.options.cxxflags)
+        if self.options.cflags != "_DUMMY_":
+            cmake.definitions["CONAN_C_FLAGS"] = cmake.definitions.get("CONAN_C_FLAGS", "") + " " + str(self.options.cflags)
+
 
         cmake.definitions["MICROARCHITECTURE"] = self.options.microarchitecture
         cmake.definitions["BITPRIM_PROJECT_VERSION"] = self.version
