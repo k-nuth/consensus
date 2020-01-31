c6e93b793d20d2e08a3d2ffffc716969863beee8
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-01-31 16:26:26 -0300

    fix build script

--- build.py
+++ build.py
@@ -2,13 +2,11 @@ import copy
 import os
 import cpuid
 import platform
-from ci_utils import get_builder, handle_microarchs, copy_env_vars, filter_valid_exts, filter_marchs_tests
+from kthbuild import get_base_march_ids, get_builder, handle_microarchs, copy_env_vars, filter_valid_exts, filter_marchs_tests
 
 if __name__ == "__main__":
-
     full_build = os.getenv('KTH_FULL_BUILD', '0') == '1'
-
-    builder, name = get_builder()
+    builder, name = get_builder(os.path.dirname(os.path.abspath(__file__)))
     builder.add_common_builds(shared_option_name="%s:shared" % name)
 
     filtered_builds = []
@@ -22,11 +20,12 @@ if __name__ == "__main__":
             if os.getenv('KTH_RUN_TESTS', 'false') == 'true':
                 options["%s:with_tests" % name] = "True"
 
-            if full_build:
-                marchs = filter_valid_exts(str(platform.system()), str(settings["compiler"]), float(str(settings["compiler.version"])), ['x86-64', 'haswell', 'skylake'])
-            else:
-                marchs = ["x86-64"]
-            marchs = ["x86-64"]
+            # if full_build:
+            #     marchs = filter_valid_exts(str(platform.system()), str(settings["compiler"]), float(str(settings["compiler.version"])), ['x86-64', 'haswell', 'skylake'])
+            # else:
+            #     marchs = ["x86-64"]
+            # marchs = ["x86-64"]
+            march_ids = get_base_march_ids()
 
             opts_bch = copy.deepcopy(options)
             opts_btc = copy.deepcopy(options)
@@ -39,10 +38,15 @@ if __name__ == "__main__":
             # opts_bch_new = copy.deepcopy(opts_bch)
             # opts_bch_new["%s:use_domain" % name] = "True"
 
-            handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_bch, env_vars, build_requires)
-            # handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_bch_new, env_vars, build_requires)
-            handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_btc, env_vars, build_requires)
-            # handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_ltc, env_vars, build_requires)
+
+            # handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_bch, env_vars, build_requires)
+            # handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_btc, env_vars, build_requires)
+            # # handle_microarchs("%s:microarchitecture" % name, marchs, filtered_builds, settings, opts_ltc, env_vars, build_requires)
+
+            handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_bch, env_vars, build_requires)
+            handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_btc, env_vars, build_requires)
+            # handle_microarchs("%s:march_id" % name, march_ids, filtered_builds, settings, opts_ltc, env_vars, build_requires)
+
             filter_marchs_tests(name, filtered_builds, ["%s:with_tests" % name])
 
     builder.builds = filtered_builds
