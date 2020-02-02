9077db182c5b6c8a62dccece381fba767cd1450d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-02-02 16:47:39 -0300

    several improvements

--- conanfile.py
+++ conanfile.py
@@ -20,9 +20,7 @@ class KnuthConsensusConan(KnuthConanFile):
 
     options = {"shared": [True, False],
                "fPIC": [True, False],
-               "with_tests": [True, False],
-               "with_java": [True, False],
-               "with_python": [True, False],
+               "tests": [True, False],
                "currency": ['BCH', 'BTC', 'LTC'],
                "microarchitecture": "ANY",
                "fix_march": [True, False],
@@ -31,13 +29,15 @@ class KnuthConsensusConan(KnuthConanFile):
                "cxxflags": "ANY",
                "cflags": "ANY",
                "glibcxx_supports_cxx11_abi": "ANY",
+               "cmake_export_compile_commands": [True, False],
     }
+
+    #    "with_java": [True, False],
+    #    "with_python": [True, False],
 
     default_options = "shared=False", \
         "fPIC=True", \
-        "with_tests=False", \
-        "with_java=False", \
-        "with_python=False", \
+        "tests=False", \
         "currency=BCH", \
         "microarchitecture=_DUMMY_",  \
         "fix_march=False", \
@@ -45,7 +45,12 @@ class KnuthConsensusConan(KnuthConanFile):
         "verbose=False", \
         "cxxflags=_DUMMY_", \
         "cflags=_DUMMY_", \
-        "glibcxx_supports_cxx11_abi=_DUMMY_"
+        "glibcxx_supports_cxx11_abi=_DUMMY_", \
+        "cmake_export_compile_commands=False"
+
+        # "with_png=False", \
+        # "with_java=False", \
+        # "with_python=False", \
 
     generators = "cmake"
     exports = "conan_*", "ci_utils/*"
@@ -80,20 +85,16 @@ class KnuthConsensusConan(KnuthConanFile):
     def package_id(self):
         KnuthConanFile.package_id(self)
 
-        self.info.options.with_tests = "ANY"
-        self.info.options.with_java = "ANY"
-        self.info.options.with_python = "ANY"
-
     def build(self):
         cmake = self.cmake_basis()
-        cmake.definitions["WITH_TESTS"] = option_on_off(self.options.with_tests)
-        cmake.definitions["WITH_JAVA"] = option_on_off(self.options.with_java)
-        cmake.definitions["WITH_PYTHON"] = option_on_off(self.options.with_python)
+        # cmake.definitions["WITH_TESTS"] = option_on_off(self.options.with_tests)
+        # cmake.definitions["WITH_JAVA"] = option_on_off(self.options.with_java)
+        # cmake.definitions["WITH_PYTHON"] = option_on_off(self.options.with_python)
 
         cmake.configure(source_dir=self.source_folder)
         cmake.build()
 
-        if self.options.with_tests:
+        if self.options.tests:
             cmake.test()
             # cmake.test(target="tests")
 
