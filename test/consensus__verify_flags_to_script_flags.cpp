357d76b94284e10b7ace95d87b4f5ca08672a64e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-06-02 22:38:00 -0300

    Never use Boost.Test again consensus__verify_flags_to_script_flags

--- test/consensus__verify_flags_to_script_flags.cpp
+++ test/consensus__verify_flags_to_script_flags.cpp
@@ -12,142 +12,144 @@
 
 using namespace kth::consensus;
 
-BOOST_AUTO_TEST_SUITE(consensus__verify_flags_to_script_flags)
+// Start Boost Suite: consensus  verify flags to script flags
 
 // Unnamed enum values require cast for boost comparison macros.
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__none__NONE)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_none), (uint32_t)SCRIPT_VERIFY_NONE);
+TEST_CASE("consensus  verify flags to script flags  none  NONE", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_none) == (uint32_t)SCRIPT_VERIFY_NONE);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__p2sh__P2SH)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_p2sh), (uint32_t)SCRIPT_VERIFY_P2SH);
+TEST_CASE("consensus  verify flags to script flags  p2sh  P2SH", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_p2sh) == (uint32_t)SCRIPT_VERIFY_P2SH);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__strictenc__STRICTENC)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_strictenc), (uint32_t)SCRIPT_VERIFY_STRICTENC);
+TEST_CASE("consensus  verify flags to script flags  strictenc  STRICTENC", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_strictenc) == (uint32_t)SCRIPT_VERIFY_STRICTENC);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__dersig__DERSIG)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_dersig), (uint32_t)SCRIPT_VERIFY_DERSIG);
+TEST_CASE("consensus  verify flags to script flags  dersig  DERSIG", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_dersig) == (uint32_t)SCRIPT_VERIFY_DERSIG);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__low_s__LOW_S)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_low_s), (uint32_t)SCRIPT_VERIFY_LOW_S);
+TEST_CASE("consensus  verify flags to script flags  low s  LOW S", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_low_s) == (uint32_t)SCRIPT_VERIFY_LOW_S);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__nulldummy__NULLDUMMY)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_nulldummy), (uint32_t)SCRIPT_VERIFY_NULLDUMMY);
+#if ! defined(KTH_CURRENCY_BCH)
+TEST_CASE("consensus  verify flags to script flags  nulldummy  NULLDUMMY", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_nulldummy) == (uint32_t)SCRIPT_VERIFY_NULLDUMMY);
+}
+#endif
+
+TEST_CASE("consensus  verify flags to script flags  sigpushonly  SIGPUSHONLY", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_sigpushonly) == (uint32_t)SCRIPT_VERIFY_SIGPUSHONLY);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__sigpushonly__SIGPUSHONLY)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_sigpushonly), (uint32_t)SCRIPT_VERIFY_SIGPUSHONLY);
+TEST_CASE("consensus  verify flags to script flags  minimaldata  MINIMALDATA", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_minimaldata) == (uint32_t)SCRIPT_VERIFY_MINIMALDATA);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__minimaldata__MINIMALDATA)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_minimaldata), (uint32_t)SCRIPT_VERIFY_MINIMALDATA);
+TEST_CASE("consensus  verify flags to script flags  discourage upgradable nops  DISCOURAGE UPGRADABLE NOPS", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_discourage_upgradable_nops) == (uint32_t)SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__discourage_upgradable_nops__DISCOURAGE_UPGRADABLE_NOPS)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_discourage_upgradable_nops), (uint32_t)SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS);
+TEST_CASE("consensus  verify flags to script flags  cleanstack  CLEANSTACK", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_cleanstack) == (uint32_t)SCRIPT_VERIFY_CLEANSTACK);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__cleanstack__CLEANSTACK)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_cleanstack), (uint32_t)SCRIPT_VERIFY_CLEANSTACK);
+TEST_CASE("consensus  verify flags to script flags  checklocktimeverify  CHECKLOCKTIMEVERIFY", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_checklocktimeverify) == (uint32_t)SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__checklocktimeverify__CHECKLOCKTIMEVERIFY)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_checklocktimeverify), (uint32_t)SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY);
+TEST_CASE("consensus  verify flags to script flags  checksequenceverify  CHECKSEQUENCEVERIFY", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_checksequenceverify) == (uint32_t)SCRIPT_VERIFY_CHECKSEQUENCEVERIFY);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__checksequenceverify__CHECKSEQUENCEVERIFY)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_checksequenceverify), (uint32_t)SCRIPT_VERIFY_CHECKSEQUENCEVERIFY);
+#if ! defined(KTH_CURRENCY_BCH)
+TEST_CASE("consensus  verify flags to script flags  witness  WITNESS", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_witness) == (uint32_t)SCRIPT_VERIFY_WITNESS);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__witness__WITNESS)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_witness), (uint32_t)SCRIPT_VERIFY_WITNESS);
+TEST_CASE("consensus  verify flags to script flags  discourage upgradable witness program  DISCOURAGE UPGRADABLE WITNESS PROGRAM", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_discourage_upgradable_witness_program) == (uint32_t)SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM);
+}
+#endif
+
+TEST_CASE("consensus  verify flags to script flags  minimal if  MINIMALIF", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_minimal_if) == (uint32_t)SCRIPT_VERIFY_MINIMALIF);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__discourage_upgradable_witness_program__DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_discourage_upgradable_witness_program), (uint32_t)SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM);
+TEST_CASE("consensus  verify flags to script flags  null fail  NULLFAIL", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_null_fail) == (uint32_t)SCRIPT_VERIFY_NULLFAIL);
 }
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__minimal_if__MINIMALIF)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_minimal_if), (uint32_t)SCRIPT_VERIFY_MINIMALIF);
+#if ! defined(KTH_CURRENCY_BCH)
+TEST_CASE("consensus  verify flags to script flags  witness public key compressed  PUBKEYTYPE", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_witness_public_key_compressed) == (uint32_t)SCRIPT_VERIFY_WITNESS_PUBKEYTYPE);
 }
+#endif
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__null_fail__NULLFAIL)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_null_fail), (uint32_t)SCRIPT_VERIFY_NULLFAIL);
+#if ! defined(KTH_CURRENCY_BCH)
+TEST_CASE("consensus  verify flags to script flags  const scriptcode  SCRIPTCODE", "[consensus  verify flags to script flags]") {
+    REQUIRE(verify_flags_to_script_flags(verify_flags_const_scriptcode) == (uint32_t)SCRIPT_VERIFY_CONST_SCRIPTCODE);
 }
+#endif
 
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__witness_public_key_compressed__PUBKEYTYPE)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_witness_public_key_compressed), (uint32_t)SCRIPT_VERIFY_WITNESS_PUBKEYTYPE);
-}
-
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__const_scriptcode__SCRIPTCODE)
-{
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(verify_flags_const_scriptcode), (uint32_t)SCRIPT_VERIFY_CONST_SCRIPTCODE);
-}
-
-
-BOOST_AUTO_TEST_CASE(consensus__verify_flags_to_script_flags__all__all)
-{
+TEST_CASE("consensus  verify flags to script flags  all  all", "[consensus  verify flags to script flags]") {
     const uint32_t all_verify_flags =
-        verify_flags_none |
-        verify_flags_p2sh |
-        verify_flags_strictenc |
-        verify_flags_dersig |
-        verify_flags_low_s |
-        verify_flags_nulldummy |
-        verify_flags_sigpushonly |
-        verify_flags_minimaldata |
-        verify_flags_discourage_upgradable_nops |
-        verify_flags_cleanstack |
-        verify_flags_checklocktimeverify |
-        verify_flags_checksequenceverify |
-        verify_flags_witness |
-        verify_flags_discourage_upgradable_witness_program |
-        verify_flags_minimal_if |
-        verify_flags_null_fail |
-        verify_flags_witness_public_key_compressed;
+          verify_flags_none
+        | verify_flags_p2sh
+        | verify_flags_strictenc
+        | verify_flags_dersig
+        | verify_flags_low_s
+#if ! defined(KTH_CURRENCY_BCH)
+        | verify_flags_nulldummy
+#endif
+        | verify_flags_sigpushonly
+        | verify_flags_minimaldata
+        | verify_flags_discourage_upgradable_nops
+        | verify_flags_cleanstack
+        | verify_flags_checklocktimeverify
+        | verify_flags_checksequenceverify
+#if ! defined(KTH_CURRENCY_BCH)
+        | verify_flags_witness
+        | verify_flags_discourage_upgradable_witness_program
+#endif
+        | verify_flags_minimal_if
+        | verify_flags_null_fail
+#if ! defined(KTH_CURRENCY_BCH)
+        | verify_flags_witness_public_key_compressed
+#endif
+        ;
 
     const uint32_t all_script_flags =
-        SCRIPT_VERIFY_NONE |
-        SCRIPT_VERIFY_P2SH |
-        SCRIPT_VERIFY_STRICTENC |
-        SCRIPT_VERIFY_DERSIG |
-        SCRIPT_VERIFY_LOW_S |
-        SCRIPT_VERIFY_NULLDUMMY |
-        SCRIPT_VERIFY_SIGPUSHONLY |
-        SCRIPT_VERIFY_MINIMALDATA |
-        SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS |
-        SCRIPT_VERIFY_CLEANSTACK |
-        SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY |
-        SCRIPT_VERIFY_CHECKSEQUENCEVERIFY |
-        SCRIPT_VERIFY_WITNESS |
-        SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM |
-        SCRIPT_VERIFY_MINIMALIF |
-        SCRIPT_VERIFY_NULLFAIL |
-        SCRIPT_VERIFY_WITNESS_PUBKEYTYPE;
+          SCRIPT_VERIFY_NONE
+        | SCRIPT_VERIFY_P2SH
+        | SCRIPT_VERIFY_STRICTENC
+        | SCRIPT_VERIFY_DERSIG
+        | SCRIPT_VERIFY_LOW_S
+#if ! defined(KTH_CURRENCY_BCH)
+        | SCRIPT_VERIFY_NULLDUMMY
+#endif
+        | SCRIPT_VERIFY_SIGPUSHONLY
+        | SCRIPT_VERIFY_MINIMALDATA
+        | SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_NOPS
+        | SCRIPT_VERIFY_CLEANSTACK
+        | SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY
+        | SCRIPT_VERIFY_CHECKSEQUENCEVERIFY
+#if ! defined(KTH_CURRENCY_BCH)
+        | SCRIPT_VERIFY_WITNESS
+        | SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_WITNESS_PROGRAM
+#endif
+        | SCRIPT_VERIFY_MINIMALIF
+        | SCRIPT_VERIFY_NULLFAIL
+#if ! defined(KTH_CURRENCY_BCH)
+        | SCRIPT_VERIFY_WITNESS_PUBKEYTYPE
+#endif
+        ;
 
-    BOOST_REQUIRE_EQUAL(verify_flags_to_script_flags(all_verify_flags), all_script_flags);
+    REQUIRE(verify_flags_to_script_flags(all_verify_flags) == all_script_flags);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// End Boost Suite
