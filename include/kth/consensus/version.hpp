c7a85a2de4e2cb3da72e2fe88eecfc83ed23f102
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-01-10 11:09:40 +0100

    Copyright update

--- include/kth/consensus/version.hpp
+++ include/kth/consensus/version.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2022 Knuth Project developers.
+// Copyright (c) 2016-2023 Knuth Project developers.
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -24,6 +24,6 @@
 // namespace kth { namespace ?????? {
 // char const* version();
 // }} /*namespace kth ????????*/
- 
+
 
 #endif //KTH_CONSENSUS_VERSION_HPP_
