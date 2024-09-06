f5d59a9ba9e518e5293373d438638602f8c3bd67
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-09-06 13:27:42 +0200

    feat: upgrade dependencies (#16)
    * upgrade dependencies
    
    * feat: upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class KnuthConsensusConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("boost/1.85.0", transitive_headers=True, transitive_libs=True)
+        self.requires("boost/1.86.0", transitive_headers=True, transitive_libs=True)
         self.requires("secp256k1/0.18.0", transitive_headers=True, transitive_libs=True)
 
         if self.settings.compiler == "msvc" and self.options.currency == 'BCH':
